8a8fd7de47b9c2fde11fc110cd9e53d8d8e0d3fc
arothma1 <arothma1@umd.edu>
2020-11-25 17:04:06 -0500

    Initial skeleton

---
+++ Project/app/src/main/java/cmsc436/semesterproject/localapparel/LoginActivity.kt
@@ -0,0 +1,79 @@
+package cmsc436.semesterproject.localapparel
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class LoginActivity : AppCompatActivity() {
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var userEmail: EditText? = null
+    private var userPassword: EditText? = null
+    private var loginBtn: Button? = null
+    private var progressBar: ProgressBar? = null
+
+    private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        userEmail = findViewById(R.id.email)
+        userPassword = findViewById(R.id.password)
+        loginBtn = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+
+        loginBtn!!.setOnClickListener { loginUserAccount() }
+    }
+
+    private fun loginUserAccount() {
+        progressBar?.visibility = View.VISIBLE
+
+        val email: String = userEmail?.text.toString()
+        val password: String = userPassword?.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                progressBar?.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
+                        .show()
+                    val successfulLoginIntent = Intent(this@LoginActivity, FeedActivity::class.java)
+                    successfulLoginIntent.putExtra(USER_ID, mAuth!!.uid)
+                    startActivity(successfulLoginIntent)
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Login failed! Please try again later",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    companion object {
+        const val USER_EMAIL = "com.example.tesla.myhomelibrary.useremail"
+        const val USER_ID = "com.example.tesla.myhomelibrary.userid"
+    }
+}
