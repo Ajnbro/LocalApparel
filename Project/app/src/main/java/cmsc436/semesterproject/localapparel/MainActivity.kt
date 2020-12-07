8ea2780ced490f5d28f7dd0e8d6d96b385b09cb8
Manpreet Sidhu <msidhu5@umd.edu>
2020-12-07 17:35:43 -0500

    Fixed Permission Request

--- Project/app/src/main/java/cmsc436/semesterproject/localapparel/MainActivity.kt
+++ Project/app/src/main/java/cmsc436/semesterproject/localapparel/MainActivity.kt
@@ -1,10 +1,16 @@
 package cmsc436.semesterproject.localapparel
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 // CITATION: Lab7 modified
 class MainActivity : AppCompatActivity() {
@@ -33,5 +39,52 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    // CITATION: Lab11
+    // Ensures that location permissions have been properly granted
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= 23 &&
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                "android.permission.ACCESS_FINE_LOCATION"
+            ) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(
+                applicationContext,
+                "android.permission.ACCESS_COARSE_LOCATION"
+            ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity, arrayOf(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION"
+                ),
+                FeedActivity.MY_PERMISSIONS_LOCATION
+            )
+        }
+    }
+
+    // CITATION: Lab11
+    // Ensures that permissions have been properly granted for Location services
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            FeedActivity.MY_PERMISSIONS_LOCATION -> {
+                var g = 0
+                for (perm in permissions)
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Must grant location permission to use app!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+                }
+            }
+        }
     }
 }
